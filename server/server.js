abaf045e9fdba637dbd78dc6708a934b95f8dcf7
Ela Piech <e-l-a-p@wp.pl>
2021-10-21 19:23:14 +0200

    Add server side rendering of messages

--- server/server.js
+++ server/server.js
@@ -13,24 +13,24 @@ const portOfServer = 3001;
 
 app.post('/:id/conv', (req,res)=>{
     console.log(req.params.id);
-    console.log(req.body.messages);
+    console.log(req.body.messages); //this is in JSON.stringify format
 
-    fs.writeFile(req.params.id+'.json', JSON.stringify(req.body.messages),  (err)=>{ if(err) {console.log('error on '+FileName)}else{console.log('send')}})
-    res.status(200).send(JSON.stringify(req.body.messages))
+    fs.writeFile(req.params.id+'.json', JSON.stringify(req.body.messages),  (err)=>{ if(err) {console.log('error on '+FileName)}else{console.log(req.body.messages)}})
+    res.status(200).send(req.body.messages)
     
 });
 
 app.get('/:id/conv', (req,res)=>{
     
     const FileName = req.params.id;
-    fs.readFile(FileName+'.json', 'utf8', (err,data)=>{ 
+    try{fs.readFile(FileName+'.json', 'utf8', (err,data)=>{ 
         if(err) {
             console.log('error on '+FileName)
        
         }else{
             res.status(200).json(data)
             console.log('send')}})
-   
+        }catch{res.status(200).json("[]")}
 });
 
 app.listen(portOfServer,() =>{ console.log('server is listening on port' + portOfServer)});
